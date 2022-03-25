0190c2d348155a81a66bfd05388646c2b135d309
xiehp <xiehp@mingyuanyun.com>
2022-03-25 22:44:04 +0800

    fix: ts error

--- components/ArticleEdit/index.tsx
+++ components/ArticleEdit/index.tsx
@@ -1,16 +1,48 @@
 import OSS from 'ali-oss';
-import { Affix, Breadcrumb, Button, Col, Input, InputNumber, message, Modal, Row, Select, Skeleton, Switch } from 'antd';
+import {
+  Affix,
+  Breadcrumb,
+  Button,
+  Col,
+  Input,
+  InputNumber,
+  message,
+  Modal,
+  Row,
+  Select,
+  Skeleton,
+  Switch,
+} from 'antd';
 import { UploadFile } from 'antd/lib/upload/interface';
 import MarkdownIt from 'markdown-it';
-import React, { FC, useCallback, useContext, useEffect, useMemo, useReducer, useRef, useState } from 'react';
+import React, {
+  FC,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useReducer,
+  useRef,
+  useState,
+} from 'react';
 import { useRouter } from 'next/router';
 import MdEditor from 'react-markdown-editor-lite';
-import { HtmlType } from 'react-markdown-editor-lite/editor/preview';
+import { HtmlType } from 'react-markdown-editor-lite/cjs/editor/preview';
 import { reducer, UPDATEARTICLE } from '../../store/articleEdit';
 import { UserContext } from '../../store/user';
-import { ArticleCategory, IArticleEntity, ITagEntity, ShareWith } from '../../types';
+import {
+  ArticleCategory,
+  IArticleEntity,
+  ITagEntity,
+  ShareWith,
+} from '../../types';
 import { AliyunOSSDir, AliyunOSSHost } from '../../utils/constant';
-import { articleGet, articleSave, stsAuth, tagList } from '../../utils/httpClient';
+import {
+  articleGet,
+  articleSave,
+  stsAuth,
+  tagList,
+} from '../../utils/httpClient';
 import { CoverUpload } from './CoverUpload';
 import { UranusPrompt } from '../UranusPrompt';
 
@@ -72,10 +104,18 @@ const ArticleEdit: FC<IArticleEditProps> = (props) => {
           return hljs.highlight(lang, code).value;
         }
         return hljs.highlightAuto(code).value;
-      }
+      },
     });
 
-    _md.use(emoji).use(mark).use(ins).use(abbr).use(footnote).use(sup).use(sub).use(mdcontainer, 'uranus-warning');
+    _md
+      .use(emoji)
+      .use(mark)
+      .use(ins)
+      .use(abbr)
+      .use(footnote)
+      .use(sup)
+      .use(sub)
+      .use(mdcontainer, 'uranus-warning');
 
     _md.renderer.rules.emoji = (token, idx) => {
       return twemoji.parse(token[idx].content);
@@ -88,61 +128,69 @@ const ArticleEdit: FC<IArticleEditProps> = (props) => {
   const [saving, setSaving] = useState(false);
   const [tags, setTags] = useState<ITagEntity[]>([]);
   const [coverPicture, setCoverPicture] = useState<UploadFile[]>([]);
-  const [state, dispatch] = useReducer<(preState: IArticleEntity | null, action: { type: string, data: IArticleEntity | null }) => IArticleEntity | null>(reducer, null);
+  const [state, dispatch] = useReducer<
+    (
+      preState: IArticleEntity | null,
+      action: { type: string; data: IArticleEntity | null }
+    ) => IArticleEntity | null
+  >(reducer, null);
 
   const unsavedChanges = useRef(false);
 
   useEffect(() => {
     if (router.query.id === 'new') {
-      Promise.all([
-        Promise.resolve<IArticleEntity>(articleInit),
-        tagList(),
-      ]).then(([article, tagsResult]) => {
-        setTags(tagsResult.data.data);
-        setCoverPicture([]);
-        dispatch({ type: UPDATEARTICLE, data: article });
-        setInitLoading(false);
-      }).catch((reason: Error) => {
-        Modal.error({
-          title: '错误',
-          content: reason.message,
+      Promise.all([Promise.resolve<IArticleEntity>(articleInit), tagList()])
+        .then(([article, tagsResult]) => {
+          setTags(tagsResult.data.data);
+          setCoverPicture([]);
+          dispatch({ type: UPDATEARTICLE, data: article });
+          setInitLoading(false);
+        })
+        .catch((reason: Error) => {
+          Modal.error({
+            title: '错误',
+            content: reason.message,
+          });
         });
-      });
     } else {
-      Promise.all([
-        articleGet(null, router.query.id as string),
-        tagList(),
-      ]).then(([articleResult, tagsResult]) => {
-        setTags(tagsResult.data.data);
-        const { article } = articleResult.data.data;
-        dispatch({ type: UPDATEARTICLE, data: article });
+      Promise.all([articleGet(null, router.query.id as string), tagList()])
+        .then(([articleResult, tagsResult]) => {
+          setTags(tagsResult.data.data);
+          const { article } = articleResult.data.data;
+          dispatch({ type: UPDATEARTICLE, data: article });
 
-        const coverPic = article.coverPicture;
-        const filename = coverPic.slice(coverPic.lastIndexOf('/'));
-        const fileType = filename.slice(filename.lastIndexOf('.'));
+          const coverPic = article.coverPicture;
+          const filename = coverPic.slice(coverPic.lastIndexOf('/'));
+          const fileType = filename.slice(filename.lastIndexOf('.'));
 
-        setCoverPicture([{
-          uid: article.createdBy,
-          size: 0,
-          name: filename,
-          type: `image/${fileType}`,
-          url: coverPic,
-          status: 'done',
-        }]);
-        setInitLoading(false);
-      }).catch((reason: Error) => {
-        Modal.error({
-          title: '错误',
-          content: reason.message,
+          setCoverPicture([
+            {
+              uid: article.createdBy,
+              size: 0,
+              name: filename,
+              type: `image/${fileType}`,
+              url: coverPic,
+              status: 'done',
+            },
+          ]);
+          setInitLoading(false);
+        })
+        .catch((reason: Error) => {
+          Modal.error({
+            title: '错误',
+            content: reason.message,
+          });
         });
-      });
     }
   }, [router.query.id]);
 
-  const onTitleChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    unsavedChanges.current = true;
-    dispatch({ type: UPDATEARTICLE, data: { title: event.target.value } });
-  }, []);
+  const onTitleChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      unsavedChanges.current = true;
+      dispatch({ type: UPDATEARTICLE, data: { title: event.target.value } });
+    },
+    []
+  );
 
   const onCoverChange = useCallback((fileList: UploadFile[]) => {
     unsavedChanges.current = true;
@@ -169,60 +217,77 @@ const ArticleEdit: FC<IArticleEditProps> = (props) => {
     dispatch({ type: UPDATEARTICLE, data: { category: value } });
   }, []);
 
-  const onDescChange = useCallback((event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    unsavedChanges.current = true;
-    dispatch({ type: UPDATEARTICLE, data: { desc: event.target.value } });
-  }, []);
+  const onDescChange = useCallback(
+    (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+      unsavedChanges.current = true;
+      dispatch({ type: UPDATEARTICLE, data: { desc: event.target.value } });
+    },
+    []
+  );
 
-  const renderHTML = useCallback<(text: string) => HtmlType | Promise<HtmlType> | (() => HtmlType)>((text) => {
-    return md.render(text);
-  }, [md]);
+  const renderHTML = useCallback<
+    (text: string) => HtmlType | Promise<HtmlType> | (() => HtmlType)
+  >(
+    (text) => {
+      return md.render(text);
+    },
+    [md]
+  );
 
   const onChargeChange = useCallback((checked: boolean) => {
     unsavedChanges.current = true;
-    dispatch({ type: UPDATEARTICLE, data: checked ? { charge: checked } : { charge: checked, amount: 0 } });
+    dispatch({
+      type: UPDATEARTICLE,
+      data: checked ? { charge: checked } : { charge: checked, amount: 0 },
+    });
   }, []);
 
-  const onAmountChange = useCallback((value: string | number | null | undefined) => {
-    unsavedChanges.current = true;
-    dispatch({ type: UPDATEARTICLE, data: { amount: value as number } });
-  }, []);
+  const onAmountChange = useCallback(
+    (value: string | number | null | undefined) => {
+      unsavedChanges.current = true;
+      dispatch({ type: UPDATEARTICLE, data: { amount: value as number } });
+    },
+    []
+  );
 
-  const onEditorChange = useCallback((data: {
-    text: string;
-    html: string;
-  }) => {
+  const onEditorChange = useCallback((data: { text: string; html: string }) => {
     unsavedChanges.current = true;
     dispatch({ type: UPDATEARTICLE, data: { content: data.text } });
   }, []);
 
-  const onImageUpload = useCallback(async (file: File) => {
-    try {
-      if (!userContext.userState) {
-        return Promise.reject('请先登录');
+  const onImageUpload = useCallback(
+    async (file: File) => {
+      try {
+        if (!userContext.userState) {
+          return Promise.reject('请先登录');
+        }
+
+        const stsResult = await stsAuth();
+        const { AccessKeyId, AccessKeySecret, SecurityToken } =
+          stsResult.data.data;
+        const client = new OSS({
+          region: 'oss-cn-beijing',
+          accessKeyId: AccessKeyId,
+          accessKeySecret: AccessKeySecret,
+          stsToken: SecurityToken,
+          bucket: 'uranus-houhou',
+          secure: true,
+        });
+
+        const suffix = file.name.slice(file.name.lastIndexOf('.'));
+        const filename = `${AliyunOSSDir}/${
+          userContext.userState.id
+        }/article_picture_${new Date().getTime()}${suffix}`;
+
+        await client.put(filename, file);
+
+        return `${AliyunOSSHost}/${filename}`;
+      } catch (ex) {
+        return 'https://img.houhoukang.com/uranus/system/upload-failed.png';
       }
-
-      const stsResult = await stsAuth();
-      const { AccessKeyId, AccessKeySecret, SecurityToken } = stsResult.data.data;
-      const client = new OSS({
-        region: 'oss-cn-beijing',
-        accessKeyId: AccessKeyId,
-        accessKeySecret: AccessKeySecret,
-        stsToken: SecurityToken,
-        bucket: 'uranus-houhou',
-        secure: true,
-      });
-
-      const suffix = file.name.slice(file.name.lastIndexOf('.'));
-      const filename = `${AliyunOSSDir}/${userContext.userState.id}/article_picture_${new Date().getTime()}${suffix}`;
-
-      await client.put(filename, file);
-
-      return `${AliyunOSSHost}/${filename}`;
-    } catch (ex) {
-      return 'https://img.houhoukang.com/uranus/system/upload-failed.png';
-    }
-  }, [userContext.userState]);
+    },
+    [userContext.userState]
+  );
 
   const onSaveClick = useCallback(async () => {
     try {
@@ -238,7 +303,10 @@ const ArticleEdit: FC<IArticleEditProps> = (props) => {
 
       if (!coverPicture.length) {
         throw new Error('文章封面不能为空');
-      } else if (coverPicture[0].url && !coverPicture[0].url.match(/\.(?:jpeg|jpg|png|webp|bmp|gif|svg)$/)) {
+      } else if (
+        coverPicture[0].url &&
+        !coverPicture[0].url.match(/\.(?:jpeg|jpg|png|webp|bmp|gif|svg)$/)
+      ) {
         throw new Error('文章封面格式不合法');
       }
 
@@ -286,19 +354,34 @@ const ArticleEdit: FC<IArticleEditProps> = (props) => {
 
   return (
     <>
-      <Breadcrumb className={props.breadcrumbClassName ? props.breadcrumbClassName : 'uranus-admin-breadcrumb'}>
+      <Breadcrumb
+        className={
+          props.breadcrumbClassName
+            ? props.breadcrumbClassName
+            : 'uranus-admin-breadcrumb'
+        }
+      >
         <Breadcrumb.Item>编辑文章</Breadcrumb.Item>
       </Breadcrumb>
       <div style={{ padding: 5, minHeight: 360, background: '#fff' }}>
         <Skeleton loading={initLoading}>
           <Row className="uranus-row" style={{ rowGap: 0 }}>
             <Col span={24}>
-              <Input placeholder="文章标题" value={state?.title} onChange={onTitleChange} />
+              <Input
+                placeholder="文章标题"
+                value={state?.title}
+                onChange={onTitleChange}
+              />
             </Col>
           </Row>
           <Row style={{ rowGap: 0 }}>
             <Col span={24}>
-              <Select placeholder="文章类别" className="uranus-row-select" value={state?.category} onChange={onCategoryChange}>
+              <Select
+                placeholder="文章类别"
+                className="uranus-row-select"
+                value={state?.category}
+                onChange={onCategoryChange}
+              >
                 <Select.Option value={ArticleCategory.frontend}>
                   前端优选
                 </Select.Option>
@@ -322,32 +405,43 @@ const ArticleEdit: FC<IArticleEditProps> = (props) => {
                 value={state?.tags}
                 onChange={onTagChange}
               >
-                {
-                  tags.map(tag => {
-                    return (
-                      <Select.Option key={tag.id} value={tag.id as string}>{tag.name}</Select.Option>
-                    );
-                  })
-                }
+                {tags.map((tag) => {
+                  return (
+                    <Select.Option key={tag.id} value={tag.id as string}>
+                      {tag.name}
+                    </Select.Option>
+                  );
+                })}
               </Select>
             </Col>
           </Row>
           <Row className="uranus-row" style={{ rowGap: 0 }}>
             <Col span={24}>
-              <Select mode="tags" className="uranus-row-select" placeholder="SEO 关键词" value={state?.keyword} onChange={handlekeywordChange}>
-                {
-                  state?.keyword?.map(kw => {
-                    return (
-                      <Select.Option key={kw} value={kw}>{kw}</Select.Option>
-                    );
-                  })
-                }
+              <Select
+                mode="tags"
+                className="uranus-row-select"
+                placeholder="SEO 关键词"
+                value={state?.keyword}
+                onChange={handlekeywordChange}
+              >
+                {state?.keyword?.map((kw) => {
+                  return (
+                    <Select.Option key={kw} value={kw}>
+                      {kw}
+                    </Select.Option>
+                  );
+                })}
               </Select>
             </Col>
           </Row>
           <Row className="uranus-row" style={{ rowGap: 0 }}>
             <Col span={24}>
-              <Select placeholder="文章可见范围" className="uranus-row-select" value={state?.shareWith} onChange={onShareWithChange}>
+              <Select
+                placeholder="文章可见范围"
+                className="uranus-row-select"
+                value={state?.shareWith}
+                onChange={onShareWithChange}
+              >
                 <Select.Option value={ShareWith.private}>
                   仅自己可见
                 </Select.Option>
@@ -375,7 +469,15 @@ const ArticleEdit: FC<IArticleEditProps> = (props) => {
                 checked={state?.charge}
                 onChange={onChargeChange}
               />
-              <InputNumber className={styles.uranus_article_amount} placeholder="付费金额 [元]" min={0.01} disabled={!state?.charge} value={state?.amount} precision={2} onChange={onAmountChange} />
+              <InputNumber
+                className={styles.uranus_article_amount}
+                placeholder="付费金额 [元]"
+                min={0.01}
+                disabled={!state?.charge}
+                value={state?.amount}
+                precision={2}
+                onChange={onAmountChange}
+              />
             </Col>
           </Row>
           <Row className="uranus-row" style={{ rowGap: 0 }}>
